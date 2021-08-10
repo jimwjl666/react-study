a96110452f6bb810b35ae50f624e91015ad2e8d4
gitwujiaolong <742265483@qq.com>
2021-08-10 19:22:16 +0800

    feat: react-dom学习

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import HookPage from './hooksComponent/h-useState'
+/* import HookPage from './hooksComponent/h-useState'
 import EHookPage from './hooksComponent/h-effect';
 import UseMemoPage from './hooksComponent/h-useMemo';
 import UserReducerPage from './hooksComponent/h-useReducer'
 import UseContextFather from './hooksComponent/h-useContext'
-import UserReactRedux from './hooksComponent/h-useSelector'
+import UserReactRedux from './hooksComponent/h-useSelector' */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <HookPage />
-        <EHookPage />
-        <UseMemoPage />
-        <UserReducerPage />
-        <UseContextFather/>
-        <UserReactRedux/>
-      </header>
+      <h1>hhhh</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+const jsx = (
+  <div>
+    <h2>111</h2>
+  </div>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  jsx,
   document.getElementById('root')
 );
 
