c1c67dab77be49694d91d35d71103861ab1033da
gitwujiaolong <742265483@qq.com>
2021-08-12 18:49:02 +0800

    feat：添加react-redux实现流程

--- example/reduce.js
+++ example/reduce.js
@@ -9,30 +9,33 @@
  */
 // reduce实现函数聚合,将最内层函数执行结果传递给最外层
   
-function f1(arg) {
-  return (argOut)=>(disArg)=>{
+/* function f1(arg) {
+  return (next)=>(action)=>{
     
-    return  disArg+ argOut('disf2') +' arg1 '
+    return  
   }
-}
+} */
 function f2(arg) {
-  return (argOut)=>(disArg)=>{
-    return disArg + argOut('disf3')+' arg2 '
+  return (next)=>(action)=>{
+    console.log(2)
+    const data = next(action) // 其实是f3中(action)=>{}
+    return data
   } 
 }
 function f3(arg) {
-  return (argOut)=>(disArg)=>{
-    // console.log(argOut) // omg
-    return disArg + ' arg3 '
+  return (next)=>(action)=>{
+    console.log(3)
+    const data = next(action)
+    return data
   } 
 }
-const chain = [f1,f2,f3].map((mid)=>mid('传入arg'))
-// console.log(chain)
+const chain = [f2,f3].map((mid)=>mid('传入arg'))
+
 function compose(...funcs){
   return funcs.reduce((a,b)=>(...args)=>a(b(...args)))
 }
-const dispatch = compose(...chain)('0mg')
-console.log(dispatch('last '))
+const dispatch = compose(...chain)((action)=>1)
+console.log(dispatch('action1'))
 
 /* 
 function test(){
@@ -44,3 +47,13 @@ let enhancer = test()
 console.log(enhancer('zs')('ls')) 
 
 */
+
+
+/* const connect = (mapStateToProps,mapDispatchToProps)=>{
+  return (Warp)=>{
+    return (props)=>{
+
+    }
+  }
+}
+connect()(comp) */
